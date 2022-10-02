4b3b8cc8bbe98aec7b2e0a78dfbd866ca4f98f63
Andrew Guy <aag43@cam.ac.uk>
2022-10-02 20:09:38 +0100

    Started rewrite of cost framework to explore more general implementations instead of just Murray-type costs.

---
+++ Vascular.Optimization/Hierarchical/CombinedCost.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vascular.Geometry;
+using Vascular.Structure;
+
+namespace Vascular.Optimization.Hierarchical
+{
+    public class CombinedCost : HierarchicalCost
+    {
+        public HierarchicalCost[] Costs { get; set; }
+        public Func<HierarchicalCost[], double> Combiner { get; set; }
+        public Func<HierarchicalCost[], double[]> Gradient { get; set; }
+
+        private double[] gradients;
+
+        public override double Cost => this.Combiner(this.Costs);
+
+        public override Vector3 PositionGradient(IMobileNode node)
+        {
+            var gradient = new Vector3();
+            for (var i = 0; i < gradients.Length; ++i)
+            {
+                gradient += this.Costs[i].PositionGradient(node) * gradients[i];
+            }
+            return gradient;
+        }
+
+        public override double FlowGradient(Branch branch)
+        {
+            var gradient = 0.0;
+            for (var i = 0; i < gradients.Length; ++i)
+            {
+                gradient += this.Costs[i].FlowGradient(branch) * gradients[i];
+            }
+            return gradient;
+        }
+
+        public override double ReducedResistanceGradient(Branch branch)
+        {
+            var gradient = 0.0;
+            for (var i = 0; i < gradients.Length; ++i)
+            {
+                gradient += this.Costs[i].ReducedResistanceGradient(branch) * gradients[i];
+            }
+            return gradient;
+        }
+
+        public override void SetCache()
+        {
+            foreach (var cost in this.Costs)
+            {
+                cost.SetCache();
+            }
+            gradients = this.Gradient(this.Costs);
+        }
+    }
+}
---
+++ Vascular.Optimization/Hierarchical/HierarchicalCost.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vascular.Geometry;
+using Vascular.Structure;
+
+namespace Vascular.Optimization.Hierarchical
+{
+    /// <summary>
+    /// Represents base type for costs such as Schreiner costs and work.
+    /// </summary>
+    public abstract class HierarchicalCost
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        public abstract Vector3 PositionGradient(IMobileNode node);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="branch"></param>
+        /// <returns></returns>
+        public abstract double FlowGradient(Branch branch);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="branch"></param>
+        /// <returns></returns>
+        public abstract double ReducedResistanceGradient(Branch branch);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public abstract void SetCache();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public abstract double Cost { get; }
+    }
+}
