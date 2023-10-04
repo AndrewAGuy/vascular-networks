fae7d85b8315fcc99926a78d5ba43197e6776088
Andrew Guy <aag43@cam.ac.uk>
2023-10-04 14:10:36 +0100

    Implemented higher order Murray's law for flow-dependent exponent

--- Vascular/Structure/Splitting/FlowContextualMurray.cs
+++ Vascular/Structure/Splitting/FlowContextualMurray.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 using System.Runtime.Serialization;
 
 namespace Vascular.Structure.Splitting
@@ -9,7 +10,7 @@ namespace Vascular.Structure.Splitting
     [DataContract]
     [KnownType(typeof(ClampedMurray))]
     [KnownType(typeof(ExponentialMurray))]
-    public abstract class FlowContextualMurray : ISplittingFunction
+    public abstract class FlowContextualMurray : ISplittingFunction, IArbitrarySplittingFunction
     {
         /// <summary>
         /// 
@@ -111,6 +112,135 @@ namespace Vascular.Structure.Splitting
             (var df1_dc1, var df2_dc1) = GroupGradient(c1, c2, Exponent(q1 + q2));
             var dc1_drs1 = 0.25 * q1 * Math.Pow(c1, -3);
             return (df1_dc1 * dc1_drs1, df2_dc1 * dc1_drs1);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="fracs"></param>
+        public void Fractions(BranchNode node, double[] fracs)
+        {
+            var e = Exponent(node.Flow);
+            var S = 0.0;
+            Span<double> c = stackalloc double[fracs.Length];
+            for (var i = 0; i < fracs.Length; ++i)
+            {
+                var di = node.Downstream[i];
+                c[i] = Math.Pow(di.Flow * di.ReducedResistance, 0.25);
+                S += Math.Pow(c[i], e);
+            }
+            S = Math.Pow(S, -1.0 / e);
+            for (var i = 0; i < fracs.Length; ++i)
+            {
+                fracs[i] = c[i] * S;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="dfi_dRj"></param>
+        public void ReducedResistanceGradient(BranchNode node, double[,] dfi_dRj)
+        {
+            var e = Exponent(node.Flow);
+            Span<double> R = stackalloc double[node.Downstream.Length];
+            Span<double> Q = stackalloc double[node.Downstream.Length];
+            for (var i = 0; i < node.Downstream.Length; ++i)
+            {
+                R[i] = node.Downstream[i].ReducedResistance;
+                Q[i] = node.Downstream[i].Flow;
+            }
+            BasicGradient(R, Q, dfi_dRj, e);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="dfi_dQj"></param>
+        public void FlowGradient(BranchNode node, double[,] dfi_dQj)
+        {
+            // Start the same, then add in exponent terms
+            var e = Exponent(node.Flow);
+            Span<double> R = stackalloc double[node.Downstream.Length];
+            Span<double> Q = stackalloc double[node.Downstream.Length];
+            for (var i = 0; i < node.Downstream.Length; ++i)
+            {
+                R[i] = node.Downstream[i].ReducedResistance;
+                Q[i] = node.Downstream[i].Flow;
+            }
+            BasicGradient(Q, R, dfi_dQj, e);
+
+            var de_dq = ExponentGradient(node.Flow);
+            ExponentGradient(Q, R, dfi_dQj, e, de_dq);
+        }
+
+        private void ExponentGradient(Span<double> Q, Span<double> R, double[,] dfi_dQj, double e, double de_dq)
+        {
+            // TODO: make split version to save time recomputing c, or make it allocated in the caller
+            Span<double> c = stackalloc double[Q.Length];
+            var a = 0.0;
+            var da_dy = 0.0;
+            for (var i = 0; i < c.Length; ++i)
+            {
+                c[i] = Math.Pow(Q[i] * R[i], 0.25);
+                var cy = Math.Pow(c[i], e);
+                a += cy;
+                da_dy += cy * Math.Log(c[i]);
+            }
+            var b = -1.0 / e;
+            var db_dy = 1.0 / (e * e);
+
+            // We add in terms of ci * dF/dy dy/dQj to dfi_dqj, where dy/dQj = dy/dQ
+            // F = a^b, where a = sum(ci^y) and b = -1/y
+            // dF/dy = b a^(b-1) da/dy + a^b log a db/dy
+            // da/dy = sum(ci^y log ci)
+            // db/dy = 1/y^2
+            var dF_dy = b * Math.Pow(a, b - 1) * da_dy + Math.Pow(a, b) * Math.Log(a) * db_dy;
+            var dF_dQj = de_dq * dF_dy;
+
+            for (var i = 0; i < Q.Length; ++i)
+            {
+                for (var j = 0; j < Q.Length; ++j)
+                {
+                    dfi_dQj[i, j] += c[i] * dF_dQj;
+                }
+            }
+        }
+
+        private void BasicGradient(Span<double> A, Span<double> B, double[,] dfi_dAj, double e)
+        {
+            Span<double> c = stackalloc double[A.Length];
+            var S = 0.0;
+            for (var i = 0; i < c.Length; ++i)
+            {
+                c[i] = Math.Pow(A[i] * B[i], 0.25);
+                S += Math.Pow(c[i], e);
+            }
+            S = Math.Pow(S, -1.0 / e);
+            for (var i = 0; i < c.Length; ++i)
+            {
+                c[i] = c[i] * S;
+            }
+
+            // dfi_dAi = fi (1-fi^y) / 4Ai
+            // fdi_dAj = -fj fi^y / 4Ai
+            for (var i = 0; i < c.Length; ++i)
+            {
+                for (var j = 0; j < c.Length; ++j)
+                {
+                    if (i == j)
+                    {
+                        dfi_dAj[i, j] = c[i] * (1 - Math.Pow(c[i], e)) * 0.25 / A[i];
+                    }
+                    else
+                    {
+                        dfi_dAj[i, j] = -c[j] * Math.Pow(c[i], e) * 0.25 / A[i];
+                    }
+                }
+            }
         }
     }
 }
